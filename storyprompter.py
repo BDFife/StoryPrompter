0200cae0ec9b2759b1ab5c74b2b1426fc130a52d
Brian Fife <bdfife@gmail.com>
2012-09-29 13:25:30 -0400

    basic hello world framework

---
+++ storyprompter.py
@@ -0,0 +1,21 @@
+"""
+StoryPrompter
+"""
+
+import os
+
+from flask import Flask
+from flask import request
+from flask import url_for, redirect
+from flask import render_template
+
+app = Flask (__name__)
+
+@app.route('/')
+def index():
+    return ('hello world!')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
