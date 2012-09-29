26004a5ae0b9b08c1a4facdfecde106a9217edae
Brian Fife <bdfife@gmail.com>
2012-09-29 17:43:58 -0400

    News works, yo.

--- storygen.py
+++ storygen.py
@@ -7,7 +7,9 @@ currently this only works for fables.
 
 import json
 import random
-import request
+import requests
+from secrets import usakey
+
 
 def generate_story(type):
     source_file = open('story_elements.json', 'r')
@@ -17,12 +19,34 @@ def generate_story(type):
     if type == "Fable":
         a = random.choice(my_data['fable_subject'])
         b = random.choice(my_data['fable_body'])
-        return "Tell me a story about: " + a + " " + b
+        return "Tell me a fable. How about " + a + " " + b + "?"
 
     if type == "News":
-        
-        return "Sorry, no news today"
+        req_vars = { 'count':5,
+                     'days':0,
+                     'page':0,
+                     'encoding':'json',
+                     'api_key':usakey(),
+                   }
+
+        r = requests.get('http://api.usatoday.com/open/articles/topnews/home', params=req_vars)
+
+        articles = r.json
+        story_list = articles.get('stories', False)
+        if story_list:
+            my_story = random.choice(story_list)
+            title = my_story.get('title', False)
+            if title: 
+                link = my_story.get('link', False)
+                if link:
+                    return "Please comment on the story <a href=" + link + ">" + title + " </a>"
+                else:
+                    return "Please comment on the story " +  title
+            else:
+                return "Hey, there was not title to the story"
+        else:
+            return "Sorry, no news today"
 
 if __name__ == '__main__':
-    print generate_story()
+    print generate_story("News")
 
