bacf4aeddb9a16c73bc9828a09fac3bc4dfff291
Brian Fife <bdfife@gmail.com>
2012-09-29 17:15:13 -0400

    Adding support for multiple story types.

--- storygen.py
+++ storygen.py
@@ -7,16 +7,22 @@ currently this only works for fables.
 
 import json
 import random
+import request
 
-def generate_story():
+def generate_story(type):
     source_file = open('story_elements.json', 'r')
     my_data = json.load(source_file)
     source_file.close()
 
-    a = random.choice(my_data['fable_subject'])
-    b = random.choice(my_data['fable_body'])
+    if type == "Fable":
+        a = random.choice(my_data['fable_subject'])
+        b = random.choice(my_data['fable_body'])
+        return "Tell me a story about: " + a + " " + b
 
-    return a + " " + b
+    if type == "News":
+        
+        return "Sorry, no news today"
 
 if __name__ == '__main__':
     print generate_story()
+
--- storyprompter.py
+++ storyprompter.py
@@ -3,6 +3,7 @@ StoryPrompter
 """
 
 import os
+import random
 
 from flask import Flask
 from flask import request
@@ -10,11 +11,14 @@ from flask import url_for, redirect
 from flask import render_template
 from storygen import generate_story
 
+genres = ["Fable", "Fable", "News"]
+
 app = Flask (__name__)
 
 @app.route('/')
 def index():
-    return render_template('prompt.html', story_type="fable", story_string=generate_story())
+    my_genre = random.choice(genres)
+    return render_template('prompt.html', story_type=my_genre, story_string=generate_story(my_genre))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
